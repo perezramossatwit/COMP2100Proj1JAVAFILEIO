3adb172e067ece615410cf2c6679b2985837a701
Sean Perez-Ramos <perezramoss@wit.edu>
2025-11-04 11:19:59 -0500

    Added JavaFX message screen!

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,42 +2,46 @@
 package application ;
 
 import javafx.application.Application ;
-import javafx.stage.Stage ;
+import javafx.fxml.FXMLLoader ;
 import javafx.scene.Scene ;
 import javafx.scene.layout.BorderPane ;
-import javafx.fxml.FXMLLoader ;
+import javafx.stage.Stage ;
 
 
 public class Main extends Application
-    {
+{
 
     @Override
     public void start( Stage primaryStage )
-        {
+    {
 
         try
-            {
+        {
+            FXMLLoader loader = new FXMLLoader( getClass().getResource( "Sample.fxml" ) ) ;
+            loader.setController( new SampleController() ) ;
             BorderPane root = (BorderPane) FXMLLoader.load( getClass().getResource( "Sample.fxml" ) ) ;
-            Scene scene = new Scene( root, 400, 400 ) ;
+            Scene scene = new Scene( root, 600, 600 ) ;
             scene.getStylesheets()
                  .add( getClass().getResource( "application.css" )
                                  .toExternalForm() ) ;
             primaryStage.setScene( scene ) ;
+            primaryStage.setMaximized( true ) ;
+            primaryStage.centerOnScreen() ;
             primaryStage.show() ;
-            }
-        catch ( Exception e )
-            {
-            e.printStackTrace() ;
-            }
-
         }
-
-
-    public static void main( String[] args )
+        catch ( Exception e )
         {
-
-        launch( args ) ;
-
+            e.printStackTrace() ;
         }
 
     }
+
+
+    public static void main( String[] args )
+    {
+
+        launch( args ) ;
+
+    }
+
+}
