0349e81c04563520b3ad8eed8bc7783f145cf452
Sean Perez-Ramos <perezramoss@wit.edu>
2025-11-18 13:15:45 -0500

    Shits was done

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -8,7 +8,7 @@ import javafx.scene.control.TextArea ;
 import javafx.scene.control.TextField ;
 
 public class SampleController
-    {
+{
 
     @FXML
     private Button send ;
@@ -20,24 +20,16 @@ public class SampleController
     private TextArea messageBox ;
 
 
-    public void initialize()
-        {
-
-        this.messageBox.setEditable( false ) ;
-
-        }
-
-
     @FXML
     private void messageSend( ActionEvent event )
-        {
+    {
 
         String userTyped = this.userMessage.getText() ;
         this.messageBox.appendText( userTyped ) ;
         this.messageBox.appendText( "\n" ) ;
         this.userMessage.clear() ;
 
-        }
-
-
     }
+
+
+}
