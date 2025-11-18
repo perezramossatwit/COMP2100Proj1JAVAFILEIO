b2c73b125b671cd8737e6101d272c7fed4983afd
Ben <Ben@10.220.43.146>
2025-11-18 11:53:14 -0500

    Imports

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -3,6 +3,7 @@ package application ;
 
 import javafx.event.ActionEvent ;
 import javafx.fxml.FXML ;
+import javafx.scene.control.Button ;
 import javafx.scene.control.TextArea ;
 import javafx.scene.control.TextField ;
 
