9641c82637e9c6d56eaa3169f980742edaeef15e
Ben <Ben@10.220.43.146>
2025-11-18 11:49:51 -0500

    Commenting

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -6,9 +6,6 @@ import javafx.fxml.FXML ;
 import javafx.scene.control.TextArea ;
 import javafx.scene.control.TextField ;
 
-import jdk.internal.org.jline.terminal.MouseEvent.Button ;
-
-
 public class SampleController
     {
 
